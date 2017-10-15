65e4512de67c94602ba5d3ea8381c0e7c060c436
Ash Blue <ash@blueashes.com>
2017-10-15 08:18:07 -0600

    Express now launches and actively listens

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
+import * as express from 'express';
 import { Database } from './controllers/databases/databases';
 
+import bodyParser = require('body-parser');
+
 class App {
+    public express: express.Application;
     public database: Database;
 
     constructor () {
         this.database = new Database();
+
+        this.express = express();
+        this.express.use(bodyParser.json());
     }
 }
 
